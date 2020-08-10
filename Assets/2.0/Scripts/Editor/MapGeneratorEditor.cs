502bb694fd8d21a84109b711d759159b5c25d5d5
Marcelo Chaves <marcelochaves17@gmail.com>
2020-08-10 19:48:48 -0300

    Create MapGeneratorEditor class

---
+++ Assets/2.0/Scripts/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGen = (MapGenerator) target;
+
+        if (DrawDefaultInspector())
+        {
+            if (mapGen.autoUpdate)
+            {
+                mapGen.GenerateMap();
+            }
+        }
+
+        if (GUILayout.Button("Generate"))
+        {
+            mapGen.GenerateMap();
+        }
+    }
+}
