04cab92fa76388f599e8974acea5551cc00c2d41
Marcelo Chaves <marcelochaves17@gmail.com>
2020-08-10 19:48:10 -0300

    Create noise class

---
+++ Assets/2.0/Scripts/Noise.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public static class Noise
+{
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    {
+        float [,] noiseMap = new float[mapHeight, mapHeight];
+
+        if (scale <= 0f)
+        {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY);
+                noiseMap[x, y] = perlinValue;
+            }
+        }
+
+        return noiseMap;
+    }
+}
