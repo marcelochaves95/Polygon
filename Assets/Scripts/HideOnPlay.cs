3f193f97597580552201b56b41400e52849d9a82
marcelochaves95 <marcelochaves17@gmail.com>
2022-04-13 21:30:08 -0300

    Updated HideOnPlay

--- Assets/Scripts/HideOnPlay.cs
+++ Assets/Scripts/HideOnPlay.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class HideOnPlay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
