c9af6bfec3cd82dce54d151bbc31852f4ac325a2
marcelochaves95 <marcelochaves17@gmail.com>
2022-04-19 02:20:18 -0300

    Created TerrainColor

---
+++ Assets/Scripts/TerrainColor.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace ProceduralTerrain
+{
+    [Serializable]
+    public class TerrainColor
+    {
+        public Color TopColor = Color.blue;
+        public Color MiddleColor = Color.red;
+        public Color BottomColor = Color.green;
+    }
+}
