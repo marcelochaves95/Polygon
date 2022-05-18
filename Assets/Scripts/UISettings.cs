06db1162800fef2168aad3e61c96bc63a2f15cb0
marcelochaves95 <marcelochaves17@gmail.com>
2022-05-17 21:44:42 -0300

    Minor refactor

---
+++ Assets/Scripts/UISettings.cs
@@ -0,0 +1,7 @@
+using System;
+
+[Serializable]
+public class UISettings
+{
+    public float Value;
+}
---
+++ Assets/Scripts/VertexSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public class VertexSettings
+{
+    public int XSize = 20;
+    public int ZSize = 20;
+    public Gradient Gradient;
+    public float MinTerrainHeight;
+    public float MaxTerrainHeight;
+}
