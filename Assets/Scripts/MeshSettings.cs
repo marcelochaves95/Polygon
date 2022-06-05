72eefca591a6bab0d24ac26ce30d418f2b0573f5
marcelochaves95 <marcelochaves17@gmail.com>
2022-06-05 16:20:27 -0300

    New values for mesh settings

--- Assets/Scripts/MeshSettings.cs
+++ Assets/Scripts/MeshSettings.cs
@@ -5,16 +5,16 @@ namespace Polygon
     [CreateAssetMenu]
     public class MeshSettings : ScriptableObject
     {
-        public bool UseFlatShading;
-        public UISettings Height;
-        public UISettings NoiseFrequency;
+        public GenerationMode GenerationMode;
+        public float Height;
+        public float NoiseFrequency;
         public VertexSettings vertexSettings;
 
         public void SetDefaultValues()
         {
-            UseFlatShading = true;
-            Height.Value = 3f;
-            NoiseFrequency.Value = 0.3f;
+            GenerationMode = GenerationMode.Flat;
+            Height = 3f;
+            NoiseFrequency = 0.3f;
         }
     }
 }
